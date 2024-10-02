95740374d12f455dd630b99942c25c352f202e02
Eero Mutka <eero.mutka@gmail.com>
2024-10-02 22:51:47 +0200

    Implemented rudimentary curve input

--- src/main.cpp
+++ src/main.cpp
@@ -391,7 +391,93 @@ static void UI_CurveDeinit(UI_Curve* curve) {
 }
 
 static void UI_ValCurveDraw(UI_Box* box) {
-	UI_DrawRect(box->computed_rect, UI_PINK);
+	// for drawing, we need the actual curve.
+	//UI_DrawRect(box->computed_rect, UI_PINK);
+	//UI_DrawBoxDefault(box);
+
+	struct RetainedData {
+		int selected_point_num;
+	};
+	RetainedData* data;
+	UI_BoxGetRetainedVar(box, UI_KEY(), &data);
+
+	UI_DrawRectLines(box->computed_rect, 2.f, UI_BLACK);
+
+	// TODO: fix this! Right now, this makes it so that the pointer must stay stable for the entire frame.
+	UI_Curve* curve = (UI_Curve*)box->draw_args_custom;
+	
+	if (UI_InputWasPressed(UI_Input_MouseLeft) && data->selected_point_num != 0) {
+		data->selected_point_num = 0;
+	}
+
+	if (UI_InputWasPressed(UI_Input_Delete) && data->selected_point_num && curve->points.count > 2) {
+		DS_ArrRemove(&curve->points, data->selected_point_num - 1);
+		data->selected_point_num = 0;
+	}
+
+	if (UI_DoubleClickedAnywhere() && UI_PointIsInRect(box->computed_rect, UI_STATE.mouse_pos)) {
+		for (int i = 0; i < curve->points.count; i++) {
+			UI_Vec2* curve_p = &curve->points[i];
+			float x = curve_p->x*(box->computed_rect.max.x - box->computed_rect.min.x) + box->computed_rect.min.x; // lerp
+			if (UI_STATE.mouse_pos.x < x) { // add before this point!
+				UI_Vec2 new_curve_p = {curve_p->x, 0.f};
+				DS_ArrInsert(&curve->points, i, new_curve_p);
+				data->selected_point_num = i + 1;
+				break;
+			}
+		}
+	}
+
+	UI_Vec2 prev_p = {0, 0};
+	float prev_curve_p_x = 0.f;
+	for (int i = 0; i < curve->points.count; i++) {
+		UI_Vec2* curve_p = &curve->points[i];
+
+		UI_Vec2 p = {
+			curve_p->x*(box->computed_rect.max.x - box->computed_rect.min.x) + box->computed_rect.min.x, // lerp
+			curve_p->y*(box->computed_rect.max.y - box->computed_rect.min.y) + box->computed_rect.min.y, // lerp
+		};
+		
+		bool is_hovered = false;
+		if (data->selected_point_num == 0) {
+			UI_Vec2 p_to_mouse = UI_SubV2(UI_STATE.mouse_pos, p);
+			is_hovered = p_to_mouse.x*p_to_mouse.x + p_to_mouse.y*p_to_mouse.y < 5.f*5.f;
+			if (is_hovered) {
+				if (UI_InputWasPressed(UI_Input_MouseLeft)) {
+					data->selected_point_num = i + 1;
+				}
+			}
+		}
+		
+		bool is_selected = data->selected_point_num == i + 1;
+		
+		if (is_selected && UI_InputIsDown(UI_Input_MouseLeft)) { // Dragging
+			p = UI_STATE.mouse_pos;
+			p.x = UI_Max(UI_Min(p.x, box->computed_rect.max.x), box->computed_rect.min.x);
+			p.y = UI_Max(UI_Min(p.y, box->computed_rect.max.y), box->computed_rect.min.y);
+
+			curve_p->x = (p.x - box->computed_rect.min.x) / (box->computed_rect.max.x - box->computed_rect.min.x); // inverse-lerp
+			curve_p->y = (p.y - box->computed_rect.min.y) / (box->computed_rect.max.y - box->computed_rect.min.y); // inverse-lerp
+		}
+
+		if (i == 0) curve_p->x = 0.f; // snap start to 0
+		if (i == curve->points.count - 1) curve_p->x = 1.f; // snap end to 1
+
+		if (curve_p->x < prev_curve_p_x) {
+			curve_p->x = prev_curve_p_x;
+		}
+		p.x = curve_p->x*(box->computed_rect.max.x - box->computed_rect.min.x) + box->computed_rect.min.x; // lerp
+
+		if (i > 0) {
+			UI_DrawLine(prev_p, p, 2.f, UI_LIME);
+		}
+
+		float r = is_hovered || is_selected ? 4.f : 3.f;
+		UI_DrawRect({UI_SubV2(p, UI_VEC2{r, r}), UI_AddV2(p, UI_VEC2{r, r})}, is_selected ? UI_WHITE : UI_LIME);
+		//UI_DrawCircle(p, r, 8, is_selected ? UI_WHITE : (is_hovered ? UI_LIGHTGRAY : UI_GRAY));
+		prev_p = p;
+		prev_curve_p_x = curve_p->x;
+	}
 }
 
 // IDEA: The curve UI could be used for arbitrary plotting and for example outputting profiling data!
@@ -400,6 +486,7 @@ static void UI_ValCurveDraw(UI_Box* box) {
 static void UI_AddValCurve(UI_Key key, UI_Curve* curve, UI_Size w, UI_Size h) {
 	UI_Box* box = UI_AddBox(key, w, h, UI_BoxFlag_DrawBorder);
 	box->draw = UI_ValCurveDraw;
+	box->draw_args_custom = curve;
 
 	// hmm... the curve pointer must stay valid for the entire frame.
 	// That sucks.
@@ -434,19 +521,31 @@ static void UpdateAndRender() {
 		static bool first_frame = true;
 		if (first_frame) {
 			UI_CurveInit(&curve, DS_HEAP);
+
+			DS_ArrPush(&curve.points, {0.f, 0.5f});
+			DS_ArrPush(&curve.points, {0.5f, 1.f});
+			DS_ArrPush(&curve.points, {1.f, 0.5f});
+
 			first_frame = false;
 		}
 	}
 
 	static bool wireframe = false;
 	UI_AddFmt(UI_KEY(), "Wireframe: %!b", &wireframe);
+	UI_AddBox(UI_KEY(), 0.f, 5.f, 0); // pad
 	UI_AddFmt(UI_KEY(), "Seed: %!u", &g_plant_params.random_seed);
+	UI_AddBox(UI_KEY(), 0.f, 5.f, 0); // pad
 	UI_AddFmt(UI_KEY(), "Max age: %!f", &g_plant_params.max_age);
+	UI_AddBox(UI_KEY(), 0.f, 5.f, 0); // pad
 	UI_AddFmt(UI_KEY(), "Vigor scale: %!f", &g_plant_params.vigor_scale);
-	
-	UI_AddFmt(UI_KEY(), "-- Apical growth curve --");
+	UI_AddBox(UI_KEY(), 0.f, 5.f, 0); // pad
+
+	UI_Box* apical_control_header = UI_AddBoxWithText(UI_KEY(), UI_SizeFlex(1.f), UI_SizeFit(), UI_BoxFlag_DrawTransparentBackground, "Apical control curve");
+	apical_control_header->draw_args = UI_DrawBoxDefaultArgsInit();
+	apical_control_header->draw_args->transparent_bg_color = UI_BLACK;
+
 	UI_AddValCurve(UI_KEY(), &curve, UI_SizeFlex(1.f), 80.f);
-	UI_AddFmt(UI_KEY(), "-------------------------");
+	UI_AddBox(UI_KEY(), 0.f, 5.f, 0); // pad
 
 	/*
 	UI_AddFmt(UI_KEY(), "Age: %!f", &g_plant_params.age);
